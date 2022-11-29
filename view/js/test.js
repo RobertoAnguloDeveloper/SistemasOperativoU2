546c353048a90d23f2d8098f7f237c99f83b6dda
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-11-28 22:10:08 -0500

    Missing to show date and time

--- view/js/test.js
+++ view/js/test.js
@@ -1,10 +1,10 @@
-var array = [ { transportnumber: '45', time: '10:28:00', date:"2017-01-16"}
-,{ transportnumber: '45', time: '10:38:00', date:"2017-01-16"}
-,{ transportnumber: '45', time: '10:48:00', date:"2017-01-16"}
-,{ transportnumber: '14', time: '10:12:00', date:"2017-01-16"}
-,{ transportnumber: '14', time: '10:24:00', date:"2017-01-16"}
-,{ transportnumber: '14', time: '10:52:00', date:"2017-01-16"}];
-array.sort(function(b,a){
+var array = [ { time: '10:28:00', date:"1000-10-10"}
+,{ time: '10:38:00', date:"1000-10-10"}
+,{ time: '10:48:00', date:"1000-10-10"}
+,{ time: '10:12:00', date:"1000-10-10"}
+,{ time: '10:24:00', date:"1000-10-10"}
+,{ time: '10:52:00', date:"1000-10-10"}];
+array.sort((a,b)=>{
   return new Date(b.date + " "+b.time) - new Date(a.date + " "+a.time);
 });
 console.log(array);
