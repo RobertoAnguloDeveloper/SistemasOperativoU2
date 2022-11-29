a60a9110d611cac6488254815f6fa3fd4cfd1a46
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-11-28 22:16:34 -0500

    Missing to show date and time - FIXED

--- view/js/test.js
+++ view/js/test.js
@@ -4,7 +4,7 @@ var array = [ { time: '10:28:00', date:"1000-10-10"}
 ,{ time: '10:12:00', date:"1000-10-10"}
 ,{ time: '10:24:00', date:"1000-10-10"}
 ,{ time: '10:52:00', date:"1000-10-10"}];
-array.sort((a,b)=>{
+array.sort((b,a)=>{
   return new Date(b.date + " "+b.time) - new Date(a.date + " "+a.time);
 });
 console.log(array);
